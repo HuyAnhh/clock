647575b2eedb80fd57a2fd7364d0a86d05e2c819
HuyAnh <nguyenhuyanh2612@gmail.com>
2022-10-18 23:22:43 +0700

    push2

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ let Day = [
 
 const timeEle = document.querySelectorAll(".times")
 
+
 setInterval(() => {
     const date = new Date;
     timeEle[0].innerText = Day[date.getDay()];
@@ -18,3 +19,34 @@ setInterval(() => {
     timeEle[3].innerText = date.getSeconds();
 }, 1000
 ) 
+
+const btnChangeBg = document.querySelector(".btnChangeBackground");
+const backgroundEle = document.querySelector(".flowerClock")
+const hiddenEle = document.querySelector(".hidden")
+
+const backgroundArr = [
+    "Flower1.jpeg",
+    "Flower2.jpeg",
+    "Flower3.jpeg",
+    "Flower4.jpeg",
+    "Flower5.jpeg",
+    "Flower6.jpeg",
+    "Flower7.jpeg",
+    "Flower8.jpeg",
+]
+
+
+btnChangeBg.addEventListener("click", () => {
+    let randomBackground = Math.floor(Math.random() * 9)
+    console.log(randomBackground)
+    backgroundEle.style.backgroundImage = 'url('+backgroundArr[randomBackground]+')'
+    hiddenEle.style.display = "none"
+
+})
+
+const btnSpecial = document.querySelector(".btnSpecial")
+
+btnSpecial.addEventListener("click", () => {
+    backgroundEle.style.backgroundImage = "none"
+    hiddenEle.style.display = "block"
+})
